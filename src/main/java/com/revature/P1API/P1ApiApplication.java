89dec4603ad0913f4c6a2469b56f2404c237f341
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-09-19 15:35:14 -0400

    intial commit

---
+++ src/main/java/com/revature/P1API/P1ApiApplication.java
@@ -0,0 +1,13 @@
+package com.revature.P1API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/P1API/P1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.P1API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
